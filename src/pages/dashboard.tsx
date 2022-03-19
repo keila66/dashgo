85d7b8f98218f4dba0431cdf21e92034d11cbdf5
Keila Beatriz <keila_bvs@outlook.com>
2022-03-19 19:17:17 -0300

    🚧 Aula 3 - Construindo interfaces do futuro - Controle de usuários

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -67,7 +67,7 @@ export default function Dashboard() {
   return (
     <Flex direction={"column"} h={"100vh"}>
       <Header />
-      <Flex w={"100%"} my={"6"} maxW={1400} mx={"auto"} px={"6"}>
+      <Flex w={"100%"} my={"6"} maxW={1480} mx={"auto"} px={"6"}>
         <Sidebar />
 
         <SimpleGrid
